db8e3cdb70bb9ccc270cb7b22909d514ffdeacd5
PEM-- <pemarchandet@gmail.com>
2014-10-11 20:27:34 +0200

    0.1.0

--- package.js
+++ package.js
@@ -1,16 +1,18 @@
 Package.describe({
   summary: "Stylesheets in CoffeeScript",
   version: "0.1.0",
+  name: "pierreeric:cssc",
   git: "https://github.com/PEM--/cssc.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.3.1');
-  api.addFiles('pierreeric:cssc.js');
+  api.use('coffeescript');
+  api.addFiles('pierreeric:cssc.coffee', 'client');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('pierreeric:cssc');
-  api.addFiles('pierreeric:cssc-tests.coffee', 'client');
+  api.addFiles('pierreeric:cssc-tests.js');
 });
